1a343f4fc249b3f4ce7762bac2d048afcb5a1e83
Ēriks Lapiņš <lapinseriks98@gmail.com>
2024-02-17 10:00:14 +0200

    econd try for remap for git push

--- lua/eriks/plugins/fugitive.lua
+++ lua/eriks/plugins/fugitive.lua
@@ -2,6 +2,6 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		vim.keymap.set("n", "<leader>gp", vim.cmd(":Git push"))
+		vim.keymap.set("n", "<leader>gp", vim.cmd(":G push"))
 	end
 }
